1122a2de08becad59ce6b07acc77a1c4845a70f1
phd051199 <PHD051199@gmail.com>
2020-06-06 18:28:19 +0700

    Delete zingmp3.js

--- Scripts/zingmp3.js
+++
@@ -1,2 +0,0 @@
-body = $response.body.replace(/"lastVipExpireTime":+\d{10}/g, "\"vip\":{\"expireTime\":926355600,\"startTime\":1572527803,\"subscription\":{\"status\":1,\"expireTime\": 926355600,\"platform\":2},\"vipType\":1}");
-$done({body});
