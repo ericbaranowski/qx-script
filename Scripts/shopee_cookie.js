6524cfcbd78c3d642953341c3f93c7133ad8772f
Pham Duy <phd051199@gmail.com>
2020-06-21 02:43:14 +0700

    Update shopee_cookie.js

--- Scripts/shopee_cookie.js
+++ Scripts/shopee_cookie.js
@@ -7,16 +7,79 @@ http-request ^https:\/\/shopee\.vn\/me\/setting max-size=0,script-path=shopee_ge
 
 MITM = shopee.vn
 */
-
+var $nobyda = nobyda();
+var date = new Date()
+if ($nobyda.isRequest) {
+  GetCookie()
+/*} else {
+  checkin()*/
+}
+function GetCookie() {
 if ($request.headers['Cookie']) {
     var headerSP = $request.headers['Cookie'];
     var cookie = $persistentStore.write(headerSP, "CookieSP");
     if (!cookie){
-      $notification.post("Shopee Cookie lỗi‼️", "", "Đăng nhập lại")
+      $nobyda.notify("Shopee Cookie lỗi‼️", "", "Đăng nhập lại")
     } else {
-      $notification.post("Shopee  Cookie done 🎉", "", "")
+      $nobyda.notify("Shopee  Cookie done 🎉", "", "")
     }
   } else {
-    $notification.post("Shopee lỗi đọc cookiee‼️", "", "Đăng nhập lại")
+    $nobyda.notify("Shopee lỗi đọc cookiee‼️", "", "Đăng nhập lại")
   }
-  $done({})
+  $nobyda.done();
+}
+
+function nobyda() {
+    const isRequest = typeof $request != "undefined"
+    const isSurge = typeof $httpClient != "undefined"
+    const isQuanX = typeof $task != "undefined"
+    const notify = (title, subtitle, message) => {
+      if (isQuanX) $notify(title, subtitle, message)
+      if (isSurge) $notification.post(title, subtitle, message)
+    }
+    const write = (value, key) => {
+      if (isQuanX) return $prefs.setValueForKey(value, key)
+      if (isSurge) return $persistentStore.write(value, key)
+    }
+    const read = (key) => {
+      if (isQuanX) return $prefs.valueForKey(key)
+      if (isSurge) return $persistentStore.read(key)
+    }
+    const adapterStatus = (response) => {
+      if (response) {
+        if (response.status) {
+          response["statusCode"] = response.status
+        } else if (response.statusCode) {
+          response["status"] = response.statusCode
+        }
+      }
+      return response
+    }
+    const get = (options, callback) => {
+      if (isQuanX) {
+        if (typeof options == "string") options = {
+          url: options
+        }
+        options["method"] = "GET"
+        $task.fetch(options).then(response => {
+          callback(null, adapterStatus(response), response.body)
+        }, reason => callback(reason.error, null, null))
+      }
+      if (isSurge) $httpClient.get(options, (error, response, body) => {
+        callback(error, adapterStatus(response), body)
+      })
+    }
+    const done = (value = {}) => {
+      if (isQuanX) isRequest ? $done(value) : null
+      if (isSurge) isRequest ? $done(value) : $done()
+    }
+    return {
+      isRequest,
+      notify,
+      write,
+      read,
+      get,
+      done
+    }
+  };
+
