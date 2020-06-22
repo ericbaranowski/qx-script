7a49fd28981cb495859e1a13cb1f528a8c539d1f
Pham Duy <phd051199@gmail.com>
2020-06-22 12:15:05 +0700

    change icon

--- QuanX/scripts_qx/github.js
+++ QuanX/scripts_qx/github.js
@@ -188,7 +188,7 @@ async function checkUpdate(item) {
       if (!item.hasOwnProperty("file_names")) {
         if (needUpdate(url, published_at)) {
           $.notify(
-            `🟢 [${name}] New submission`,
+            `🔰 [${name}] New submission`,
             "",
             `⏰ Submitted on: ${formatTime(
               published_at
