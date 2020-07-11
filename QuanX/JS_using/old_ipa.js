46a100a890f2d661da80ff5f7e675258f7d3dc9f
Phạm Duy <PHD051199@gmail.com>
2020-07-11 22:13:37 +0700

    Update old_ipa.js

--- QuanX/JS_using/old_ipa.js
+++ QuanX/JS_using/old_ipa.js
@@ -3,14 +3,14 @@ Old_iPA_Downloader by LangKhach
 */
 
 var url = $request.url;
-var obj = $response.body;
+var obj = $request.body;
 
 const api= "unlimapps";
 const buy= "buyProduct";
 
 if(url.indexOf(api) != -1){
 var appidget = url.match(/\d{6,}$/);
-console.log("🟢\n appid: " + appidget);
+console.log("appid: " + appidget);
 $prefs.setValueForKey(appidget.toString(),"appid");
 $notify('Old_iPA_Dowloader', 'iTunes PC search app and click Get', 'By @Lãng Khách');
 //$done({body});
@@ -18,7 +18,7 @@ $notify('Old_iPA_Dowloader', 'iTunes PC search app and click Get', 'By @Lãng Kh
 if(url.indexOf(buy) != -1){ 
 var appid= $prefs.valueForKey("appid");
 var body= obj.replace(/\d{6,}/, appid);
-console.log('🟢 Old_iPA_Downloader \nappid: ' + appid);
+console.log('Old_iPA_Downloader \nappid: ' + appid);
 $notify("Old_iPA_Downloader rewrite stustus: OK","","");
 //$done({body});
 }
