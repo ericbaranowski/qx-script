23368993ea7b0bd5043c9dbc7a9b4a6a9440389e
Phạm Duy <PHD051199@gmail.com>
2020-07-11 21:44:39 +0700

    old_ipa.js

--- QuanX/JS_using/old_ipa.js
+++ QuanX/JS_using/old_ipa.js
@@ -22,14 +22,14 @@ const buy= "buyProduct";
 if(url.indexOf(api) != -1){
 var appidget = url.match(/\d{6,}$/);
 console.log("🟢\n appid: " + appidget);
-$persistentStore.write(appidget.toString(),"appid");
-$notification.post('Old_iPA_Dowloader', 'iTunes PC search app and click Get', 'By @Lãng Khách');
-$done({body});
+//$persistentStore.write(appidget.toString(),"appid");
+//$notification.post('Old_iPA_Dowloader', 'iTunes PC search app and click Get', 'By @Lãng Khách');
+//$done({body});
 }
 if(url.indexOf(buy) != -1){ 
 var appid= $persistentStore.read("appid");
 var body= obj.replace(/\d{6,}/, appid);
 console.log('🟢 Old_iPA_Downloader \nappid: ' + appid);
-$notification.post("Old_iPA_Downloader rewrite stustus: OK","","");
-$done({body});
+//$notification.post("Old_iPA_Downloader rewrite stustus: OK","","");
+//$done({body});
 }
