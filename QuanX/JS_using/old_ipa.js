8f5c4d4f693b8a0ae48e37c67c0ed488de1c8067
Phạm Duy <PHD051199@gmail.com>
2020-07-11 22:02:33 +0700

    Update old_ipa.js

--- QuanX/JS_using/old_ipa.js
+++ QuanX/JS_using/old_ipa.js
@@ -11,14 +11,14 @@ const buy= "buyProduct";
 if(url.indexOf(api) != -1){
 var appidget = url.match(/\d{6,}$/);
 console.log("🟢\n appid: " + appidget);
-$prefs.valueForKey(appidget.toString(),"appid");
+$prefs.setValueForKey(appidget.toString(),"appid");
 $notify('Old_iPA_Dowloader', 'iTunes PC search app and click Get', 'By @Lãng Khách');
-$done({body});
+//$done({body});
 }
 if(url.indexOf(buy) != -1){ 
-var appid= $persistentStore.read("appid");
+var appid= $prefs.valueForKey("appid");
 var body= obj.replace(/\d{6,}/, appid);
 console.log('🟢 Old_iPA_Downloader \nappid: ' + appid);
 $notify("Old_iPA_Downloader rewrite stustus: OK","","");
-$done({body});
+//$done({body});
 }
