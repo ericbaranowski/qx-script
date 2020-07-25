cbddaee1f4bd50ed332630c2f14f269e05e42bdc
Phạm Duy <PHD051199@gmail.com>
2020-07-25 14:51:16 +0700

    Update DataFlow.js

--- QuanX/scripting/DataFlow.js
+++ QuanX/scripting/DataFlow.js
@@ -1,6 +1,4 @@
-//Thông tin đăng nhập
 const $nobyda = nobyda();
-
 const account = {
   user: "0354353735",
   pass: "1234qwer",
@@ -14,13 +12,10 @@ var apiloginmobile = {
   body: body,
 };
 
-//
-// async function launch() {
-//   await loginmobile();
-// }
-//launch()
-
-loginmobile()
+async function launch() {
+  await loginmobile();
+}
+launch()
 
 function loginmobile() {
   $nobyda.post(apiloginmobile, function (error, response, data) {
@@ -76,77 +71,77 @@ function nobyda() {
   const isSurge = typeof $httpClient != "undefined";
   const isQuanX = typeof $task != "undefined";
   const notify = (title, subtitle, message) => {
-      if (isQuanX) $notify(title, subtitle, message);
-      if (isSurge) $notification.post(title, subtitle, message);
+    if (isQuanX) $notify(title, subtitle, message);
+    if (isSurge) $notification.post(title, subtitle, message);
   };
   const write = (value, key) => {
-      if (isQuanX) return $prefs.setValueForKey(value, key);
-      if (isSurge) return $persistentStore.write(value, key);
+    if (isQuanX) return $prefs.setValueForKey(value, key);
+    if (isSurge) return $persistentStore.write(value, key);
   };
   const read = (key) => {
-      if (isQuanX) return $prefs.valueForKey(key);
-      if (isSurge) return $persistentStore.read(key);
+    if (isQuanX) return $prefs.valueForKey(key);
+    if (isSurge) return $persistentStore.read(key);
   };
   const adapterStatus = (response) => {
-      if (response) {
-          if (response.status) {
-              response["statusCode"] = response.status;
-          } else if (response.statusCode) {
-              response["status"] = response.statusCode;
-          }
+    if (response) {
+      if (response.status) {
+        response["statusCode"] = response.status;
+      } else if (response.statusCode) {
+        response["status"] = response.statusCode;
       }
-      return response;
+    }
+    return response;
   };
   const get = (options, callback) => {
-      if (isQuanX) {
-          if (typeof options == "string")
-              options = {
-                  url: options,
-              };
-          options["method"] = "GET";
-          $task.fetch(options).then(
-              (response) => {
-                  callback(null, adapterStatus(response), response.body);
-              },
-              (reason) => callback(reason.error, null, null)
-          );
-      }
-      if (isSurge)
-          $httpClient.get(options, (error, response, body) => {
-              callback(error, adapterStatus(response), body);
-          });
+    if (isQuanX) {
+      if (typeof options == "string")
+        options = {
+          url: options,
+        };
+      options["method"] = "GET";
+      $task.fetch(options).then(
+        (response) => {
+          callback(null, adapterStatus(response), response.body);
+        },
+        (reason) => callback(reason.error, null, null)
+      );
+    }
+    if (isSurge)
+      $httpClient.get(options, (error, response, body) => {
+        callback(error, adapterStatus(response), body);
+      });
   };
   const post = (options, callback) => {
-      if (isQuanX) {
-          if (typeof options == "string")
-              options = {
-                  url: options,
-              };
-          options["method"] = "POST";
-          $task.fetch(options).then(
-              (response) => {
-                  callback(null, adapterStatus(response), response.body);
-              },
-              (reason) => callback(reason.error, null, null)
-          );
-      }
-      if (isSurge) {
-          $httpClient.post(options, (error, response, body) => {
-              callback(error, adapterStatus(response), body);
-          });
-      }
+    if (isQuanX) {
+      if (typeof options == "string")
+        options = {
+          url: options,
+        };
+      options["method"] = "POST";
+      $task.fetch(options).then(
+        (response) => {
+          callback(null, adapterStatus(response), response.body);
+        },
+        (reason) => callback(reason.error, null, null)
+      );
+    }
+    if (isSurge) {
+      $httpClient.post(options, (error, response, body) => {
+        callback(error, adapterStatus(response), body);
+      });
+    }
   };
   const done = (value = {}) => {
-      if (isQuanX) isRequest ? $done(value) : null;
-      if (isSurge) isRequest ? $done(value) : $done();
+    if (isQuanX) isRequest ? $done(value) : null;
+    if (isSurge) isRequest ? $done(value) : $done();
   };
   return {
-      isRequest,
-      notify,
-      write,
-      read,
-      get,
-      post,
-      done,
+    isRequest,
+    notify,
+    write,
+    read,
+    get,
+    post,
+    done,
   };
 }
