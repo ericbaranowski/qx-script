11dbb1dac8581045db1f75d8a7b70fef403e6327
Phạm Duy <PHD051199@gmail.com>
2020-07-26 12:00:30 +0700

    Update DataFlow.js

--- QuanX/scripting/DataFlow.js
+++ QuanX/scripting/DataFlow.js
@@ -4,7 +4,7 @@ const account = {
   pass: "1234qwer",
 };
 
-var body = "account=" + account.user + "&build_code=2020.4.15.2&cmnd=&device_id=00000000-0000-0000-0000-000000000000&device_name=L%20ng%20Kh%20ch%20s%20iPhone%20%28iPhone%20X%29&keyDeviceAcc=xxx&os_type=ios&os_version=13.300000&password=" + account.pass + "&version_app=4.3.4";
+var body = "account=" + account.user + "&build_code=2020.4.15.2&cmnd=&device_id=00000000-0000-0000-0000-000000000000&device_name=duy&keyDeviceAcc=xxx&os_type=ios&os_version=13.600000&password=" + account.pass + "&version_app=4.3.4";
 
 var apiloginmobile = {
   url: 'https://apivtp.vietteltelecom.vn:6768/myviettel.php/loginMobile',
@@ -40,7 +40,7 @@ function loginmobile() {
 }
 
 function getdataremain(token) {
-  var body = "build_code=2020.4.15.2&device_id=00000000-0000-0000-0000-000000000000&device_name=L%20ng%20Kh%20ch%20s%20iPhone%20%28iPhone%20X%29&os_type=ios&os_version=13.300000&token=" + token + "&version_app=4.3.4";
+  var body = "build_code=2020.4.15.2&device_id=00000000-0000-0000-0000-000000000000&device_name=duy&os_type=ios&os_version=13.600000&token=" + token + "&version_app=4.3.4";
   var dataremain = {
     url: 'https://apivtp.vietteltelecom.vn:6768/myviettel.php/getDataRemain',
     headers: {},
