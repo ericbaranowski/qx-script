b7465a6cdc5e6351212a6567879ac3c57ed481b3
Phạm Duy <PHD051199@gmail.com>
2020-07-27 11:42:01 +0700

    ncov_qx.js

--- QuanX/scripting/ncov_qx.js
+++ QuanX/scripting/ncov_qx.js
@@ -1,137 +1,101 @@
-//Smart Qx&Surge 
-let isQuantumultX = $task !== undefined;
-let isSurge = $httpClient !== undefined;
-var $task = isQuantumultX ? $task : {};
-var $httpClient = isSurge ? $httpClient : {};
-var $prefs = isQuantumultX ? $prefs : {};
-var $persistentStore = isSurge ? $persistentStore : {};
-var $notify = isQuantumultX ? $notify : {};
-var $notification = isSurge ? $notification : {};
-if (isQuantumultX) {
-    var errorInfo = {
-        error: ''
+var $nobyda=nobyda();
+
+function nobyda() {
+    const isRequest = typeof $request != "undefined";
+    const isSurge = typeof $httpClient != "undefined";
+    const isQuanX = typeof $task != "undefined";
+    const notify = (title, subtitle, message) => {
+        if (isQuanX) $notify(title, subtitle, message);
+        if (isSurge) $notification.post(title, subtitle, message);
     };
-    $httpClient = {
-        get: (url, cb) => {
-            var urlObj;
-            if (typeof (url) == 'string') {
-                urlObj = {
-                    url: url
-                }
-            } else {
-                urlObj = url;
+    const write = (value, key) => {
+        if (isQuanX) return $prefs.setValueForKey(value, key);
+        if (isSurge) return $persistentStore.write(value, key);
+    };
+    const read = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key);
+        if (isSurge) return $persistentStore.read(key);
+    };
+    const adapterStatus = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status;
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode;
             }
-            $task.fetch(urlObj).then(response => {
-                cb(undefined, response, response.body)
-            }, reason => {
-                errorInfo.error = reason.error;
-                cb(errorInfo, response, '')
-            })
-        },
-        post: (url, cb) => {
-            var urlObj;
-            if (typeof (url) == 'string') {
-                urlObj = {
-                    url: url
-                }
-            } else {
-                urlObj = url;
-            }
-            url.method = 'POST';
-            $task.fetch(urlObj).then(response => {
-                cb(undefined, response, response.body)
-            }, reason => {
-                errorInfo.error = reason.error;
-                cb(errorInfo, response, '')
-            })
         }
-    }
-}
-if (isSurge) {
-    $task = {
-        fetch: url => {
-            return new Promise((resolve, reject) => {
-                if (url.method == 'POST') {
-                    $httpClient.post(url, (error, response, data) => {
-                        response.body = data;
-                        resolve(response, {
-                            error: error
-                        });
-                    })
-                } else {
-                    $httpClient.get(url, (error, response, data) => {
-                        response.body = data;
-                        resolve(response, {
-                            error: error
-                        });
-                    })
-                }
-            })
-
+        return response;
+    };
+    const get = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string")
+                options = {
+                    url: options,
+                };
+            options["method"] = "GET";
+            $task.fetch(options).then(
+                (response) => {
+                    callback(null, adapterStatus(response), response.body);
+                },
+                (reason) => callback(reason.error, null, null)
+            );
         }
-    }
-}
-if (isQuantumultX) {
-    $persistentStore = {
-        read: key => {
-            return $prefs.valueForKey(key);
-        },
-        write: (val, key) => {
-            return $prefs.setValueForKey(val, key);
+        if (isSurge)
+            $httpClient.get(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body);
+            });
+    };
+    const post = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string")
+                options = {
+                    url: options,
+                };
+            options["method"] = "POST";
+            $task.fetch(options).then(
+                (response) => {
+                    callback(null, adapterStatus(response), response.body);
+                },
+                (reason) => callback(reason.error, null, null)
+            );
         }
-    }
-}
-if (isSurge) {
-    $prefs = {
-        valueForKey: key => {
-            return $persistentStore.read(key);
-        },
-        setValueForKey: (val, key) => {
-            return $persistentStore.write(val, key);
+        if (isSurge) {
+            $httpClient.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body);
+            });
         }
-    }
+    };
+    const done = (value = {}) => {
+        if (isQuanX) isRequest ? $done(value) : null;
+        if (isSurge) isRequest ? $done(value) : $done();
+    };
+    return {
+        isRequest,
+        notify,
+        write,
+        read,
+        get,
+        post,
+        done,
+    };
 }
-if (isQuantumultX) {
-    $notification = {
-        post: (title, subTitle, detail) => {
-            $notify(title, subTitle, detail);
-        }
-    }
-}
-if (isSurge) {
-    $notify = function (title, subTitle, detail) {
-        $notification.post(title, subTitle, detail);
-    }
-}
-//end
-
-/*
-Surge V4 ios, Mac
-Api by junookyo
-
-[Script]
-cron "0 0 6-23/3 * * *" script-path=ncov.js
-
-
-MITM = code.junookyo.xyz
-*/
 var ncovUrl = {
     url: 'https://code.junookyo.xyz/api/ncov-moh/data.json',
 }
-$httpClient.get(ncovUrl, function (error, response, data) {
+$nobyda.get(ncovUrl, function (error, response, data) {
     if (error) {
-        $notification.post("NCOV", "", "Bad connection")
-        $done();
+        $nobyda.post("NCOV", "", "Bad connection")
+        $nobyda.done();
     } else {
         if (response.statusCode == 200) {
             let obj = JSON.parse(data);
             if (obj["success"]) {
                 obj = obj["data"];
-                $notification.post("Covid-19 ", "", "🇻🇳 VN: Số người nhiễm: " + obj["vietnam"]["cases"] + ", Người chết: " + obj["vietnam"]["deaths"] + ", Hồi phục: " + obj["vietnam"]["recovered"] + "\n🌍 Global:  Số người nhiễm: " + obj["global"]["cases"] + ", Người chết: " + obj["global"]["deaths"] + ", Hồi phục: " + obj["global"]["recovered"]);
-                $done();
+                $nobyda.notify("NCOV ", "", "🇻🇳 VN: Số người nhiễm: " + obj["vietnam"]["cases"] + ", Người chết: " + obj["vietnam"]["deaths"] + ", Hồi phục: " + obj["vietnam"]["recovered"] + "\n🌍 Global:  Số người nhiễm: " + obj["global"]["cases"] + ", Người chết: " + obj["global"]["deaths"] + ", Hồi phục: " + obj["global"]["recovered"]);
+                $nobyda.done();
             }
         } else {
-            $notification.post("NCOV", "", "API ERROR");
+            $nobyda.notify("NCOV", "", "API ERROR");
         }
     }
 });
