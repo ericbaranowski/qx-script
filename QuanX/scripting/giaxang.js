d2d34e165d35d46a252af136ff6edcbde867dd2c
Phạm Duy <PHD051199@gmail.com>
2020-07-28 19:28:27 +0700

    Update giaxang.js

--- QuanX/scripting/giaxang.js
+++ QuanX/scripting/giaxang.js
@@ -1,15 +1,93 @@
-﻿var wurl = {
+﻿var $ = env();
+function env() {
+  const isRequest = typeof $request != "undefined";
+  const isSurge = typeof $httpClient != "undefined";
+  const isQuanX = typeof $task != "undefined";
+  const notify = (title, subtitle, message) => {
+    if (isQuanX) $notify(title, subtitle, message);
+    if (isSurge) $notification.post(title, subtitle, message);
+  };
+  const write = (value, key) => {
+    if (isQuanX) return $prefs.setValueForKey(value, key);
+    if (isSurge) return $persistentStore.write(value, key);
+  };
+  const read = (key) => {
+    if (isQuanX) return $prefs.valueForKey(key);
+    if (isSurge) return $persistentStore.read(key);
+  };
+  const adapterStatus = (response) => {
+    if (response) {
+      if (response.status) {
+        response["statusCode"] = response.status;
+      } else if (response.statusCode) {
+        response["status"] = response.statusCode;
+      }
+    }
+    return response;
+  };
+  const get = (options, callback) => {
+    if (isQuanX) {
+      if (typeof options == "string")
+        options = {
+          url: options,
+        };
+      options["method"] = "GET";
+      $task.fetch(options).then(
+        (response) => {
+          callback(null, adapterStatus(response), response.body);
+        },
+        (reason) => callback(reason.error, null, null)
+      );
+    }
+    if (isSurge)
+      $httpClient.get(options, (error, response, body) => {
+        callback(error, adapterStatus(response), body);
+      });
+  };
+  const post = (options, callback) => {
+    if (isQuanX) {
+      if (typeof options == "string")
+        options = {
+          url: options,
+        };
+      options["method"] = "POST";
+      $task.fetch(options).then(
+        (response) => {
+          callback(null, adapterStatus(response), response.body);
+        },
+        (reason) => callback(reason.error, null, null)
+      );
+    }
+    if (isSurge) {
+      $httpClient.post(options, (error, response, body) => {
+        callback(error, adapterStatus(response), body);
+      });
+    }
+  };
+  const done = (value = {}) => {
+    if (isQuanX) isRequest ? $done(value) : null;
+    if (isSurge) isRequest ? $done(value) : $done();
+  };
+  return {
+    isRequest,
+    notify,
+    write,
+    read,
+    get,
+    post,
+    done,
+  };
+}
+var wurl = {
   url: "https://api-qtx.000webhostapp.com/giaxang.php",
 };
 var today = new Date();
-
 Giaxang();
-
 function Giaxang() {
   $task.fetch(wurl).then(
     (response) => {
       var obj = JSON.parse(response.body);
-      $notify(
+      $.notify(
         "Giá xăng 🇻🇳 " +
         addZero(today.getDate()) +
         "/" +
@@ -31,11 +109,10 @@ function Giaxang() {
       );
     },
     (reason) => {
-      $notify("False!", "", reason.error);
+      $.notify("False!", "", reason.error);
     }
   );
 }
-
 function addZero(i) {
   if (i < 10) {
     i = "0" + i;
