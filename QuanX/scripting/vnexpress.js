4f6198755a573501c71ca5559ae56676db45c4b0
Phạm Duy <PHD051199@gmail.com>
2020-07-29 23:22:36 +0700

    update

---
+++ QuanX/scripting/vnexpress.js
@@ -0,0 +1,12 @@
+var wurl = {
+    url: "https://api-qtx.000webhostapp.com/vnexpress.php",
+};
+$task.fetch(wurl).then(
+    (response) => {
+        var obj = JSON.parse(response.body);
+        $notify(":newspaper: VN[E]XPRESS.NET", obj.elements[0].title, ":calendar: "+obj.elements[0].time+"\n"+obj.elements[0].content, { "open-url": obj.elements[0].link, "media-url": obj.elements[0].video });
+    },
+    (reason) => {
+        $notify("False!", "", reason.error);
+    }
+);
