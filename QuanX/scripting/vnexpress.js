a410c69c8f5602f48c279717fe56ab937fc2c4cb
Phạm Duy <PHD051199@gmail.com>
2020-07-29 23:29:42 +0700

    vnexpress.js

--- QuanX/scripting/vnexpress.js
+++ QuanX/scripting/vnexpress.js
@@ -4,7 +4,7 @@ var wurl = {
 $task.fetch(wurl).then(
     (response) => {
         var obj = JSON.parse(response.body);
-        $notify("⏰VN[E]XPRESS.NET", obj.elements[0].title, "📆 "+obj.elements[0].time+"\n"+obj.elements[0].content, { "open-url": obj.elements[0].link, "media-url": obj.elements[0].video });
+        $notify("📰 VN[E]XPRESS.NET", "📌 "+obj.elements[0].title, "📆 "+obj.elements[0].time+"\n"+obj.elements[0].content, { "open-url": obj.elements[0].link, "media-url": obj.elements[0].video });
     },
     (reason) => {
         $notify("False!", "", reason.error);
