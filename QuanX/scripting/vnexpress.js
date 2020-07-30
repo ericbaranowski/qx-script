b79b3446bfe7876fd13848cb1e54e311dc3a75c0
Phạm Duy <PHD051199@gmail.com>
2020-07-30 20:46:23 +0700

    Update vnexpress.js

--- QuanX/scripting/vnexpress.js
+++ QuanX/scripting/vnexpress.js
@@ -1,7 +1,7 @@
 //so luong tin
-var amount = 5;
+var count = 5;
 var wurl = {
-    url: "https://api3.vnexpress.net/api/article?type=get_article_folder&cate_id=1003834&limit=" + amount + "&offset=0&option=video_autoplay,object,get_zone&app_id=9e304d",
+    url: "https://api3.vnexpress.net/api/article?type=get_article_folder&cate_id=1003834&limit=" + count + "&offset=0&option=video_autoplay,object,get_zone&app_id=9e304d",
 };
 $task.fetch(wurl).then(
     (response) => {
@@ -17,7 +17,6 @@ $task.fetch(wurl).then(
                 "media-url": obj.data["1003834"][i].check_object.video_autoplay[videoid].size_format["240"]
             }
             if (needUpdate(url_news, updatetime)) {
-                //console.log(updatetime + "\n" + url_news);
                 $notify("VN[E]XPRESS.NET" + "📆" + updatetime,
                     "📌" + titled, data_lead, notificationURL);
                 $prefs.setValueForKey(updatetime, hash(url_news));
