939a7ea3c76e1a80c27becab0d0e3e4f470ab4eb
Phạm Duy <PHD051199@gmail.com>
2020-07-30 20:32:12 +0700

    Update vnexpress.js

--- QuanX/scripting/vnexpress.js
+++ QuanX/scripting/vnexpress.js
@@ -32,14 +32,14 @@ $task.fetch(wurl).then(
 //xu ly time
 function timeConverter(UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
-    var months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+    var months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
     var hour = a.getHours();
     var min = a.getMinutes();
     var sec = a.getSeconds();
-    var time = addZero(date) + '/' + addZero(month) + '/' + year + ' ' + addZero(hour) + ':' + addZero(min);
+    var time = date + '/' + month + '/' + year + ' ' + addZero(hour) + ':' + addZero(min);
     return time;
 }
 function addZero(i) {
