6af259e20fb7392658216d8b391851b0c7f6e728
Phạm Duy <PHD051199@gmail.com>
2020-07-30 19:48:04 +0700

    Update vnexpress.js

--- QuanX/scripting/vnexpress.js
+++ QuanX/scripting/vnexpress.js
@@ -1,20 +1,39 @@
 //so luong tin
 var amount = 3;
-var wurl = {
-    url: "https://api3.vnexpress.net/api/article?type=get_article_folder&cate_id=1003834&limit=" + amount + "&offset=0&option=video_autoplay,object,get_zone&app_id=9e304d",
-};
-$task.fetch(wurl).then(
-    (response) => {
-        var obj = JSON.parse(response.body);
-        for (i = 0; i < amount; i++) {
-            var videoid = obj.data["1003834"][i].check_object.video;
-            $notify("VN[E]XPRESS.NET" + "📆" + timeConverter(obj.data["1003834"][i].publish_time), "📌" + obj.data["1003834"][i].title, obj.data["1003834"][i].lead, { "open-url": obj.data["1003834"][i].share_url, "media-url": obj.data["1003834"][i].check_object.video_autoplay[videoid].size_format["240"] });
+
+checkupdate();
+//main
+async function checkupdate() {
+    var wurl = {
+        url: "https://api3.vnexpress.net/api/article?type=get_article_folder&cate_id=1003834&limit=" + amount + "&offset=0&option=video_autoplay,object,get_zone&app_id=9e304d",
+    };
+    $task.fetch(wurl).then(
+        (response) => {
+            var obj = JSON.parse(response.body);
+            for (i = 0; i < amount; i++) {
+                const updatetime = timeConverter(obj.data["1003834"][i].publish_time);
+                const title = obj.data["1003834"][i].title;
+                const videoid = obj.data["1003834"][i].check_object.video;
+                const data_lead = obj.data["1003834"][i].lead;
+                const url_news = obj.data["1003834"][i].share_url;
+                const notificationURL = {
+                    "open-url": url_news,
+                    "media-url": obj.data["1003834"][i].check_object.video_autoplay[videoid].size_format["240"]
+                }
+                if (needUpdate(url_news, updatetime)) {
+                    $notify("VN[E]XPRESS.NET" + "📆" + updatetime,
+                        "📌" + title, data_lead, notificationURL);
+                    $prefs.setValueForKey(updatetime, hash(url_news));
+                }
+            }
+        },
+        (reason) => {
+            $notify("False!", "", reason.error);
         }
-    },
-    (reason) => {
-        $notify("False!", "", reason.error);
-    }
-);
+    );
+}
+
+//xu ly time
 function addZero(i) {
     if (i < 10) {
         i = "0" + i;
@@ -33,3 +52,22 @@ function timeConverter(UNIX_timestamp) {
     var time = date + '/' + month + '/' + year + ' ' + addZero(hour) + ':' + addZero(min);
     return time;
 }
+function hash(str) {
+    let h = 0,
+        i,
+        chr;
+    for (i = 0; i < str.length; i++) {
+        chr = str.charCodeAt(i);
+        h = (h << 5) - h + chr;
+        h |= 0; // Convert to 32bit integer
+    }
+    return String(h);
+}
+function needUpdate(url, timestamp) {
+    const storedTimestamp = $.read(hash(url));
+    $.log(`Stored Timestamp for ${hash(url)}: ` + storedTimestamp);
+    return storedTimestamp === undefined || storedTimestamp !== timestamp
+        ? true
+        : false;
+}
+
