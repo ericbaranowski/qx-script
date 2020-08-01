2aae5d44cde16cbbe15aaf1ae8d034bb5971ca8f
Phạm Duy <PHD051199@gmail.com>
2020-08-01 07:30:25 +0700

    Update giaxang.js

--- QuanX/scripting/giaxang.js
+++ QuanX/scripting/giaxang.js
@@ -2,39 +2,13 @@
   url: "https://api-qtx.000webhostapp.com/giaxang.php",
 };
 var today = new Date();
-
 $task.fetch(wurl).then(
   (response) => {
     var obj = JSON.parse(response.body);
-    $notify(
-      "Giá xăng 🇻🇳 " +
-      addZero(today.getDate()) +
-      "/" +
-      addZero(today.getMonth() + 1) +
-      "/" +
-      today.getFullYear(),
-      "",
-      obj.elements[0].title +
-      ": " +
-      obj.elements[0].subtitle +
-      "\n" +
-      obj.elements[1].title +
-      ": " +
-      obj.elements[1].subtitle +
-      "\n" +
-      obj.elements[2].title +
-      ": " +
-      obj.elements[2].subtitle
-    );
+    $notify("Giá xăng 🇻🇳 " + addZero(today.getDate()) + "/" + addZero(today.getMonth() + 1) + "/" + today.getFullYear(), "", obj.elements[0].title + ": " + obj.elements[0].subtitle + "\n" + obj.elements[1].title + ": " + obj.elements[1].subtitle + "\n" + obj.elements[2].title + ": " + obj.elements[2].subtitle);
   },
   (reason) => {
-    $notify("False!", "", reason.error);
+    $notify("False!", reason.error);
   }
 );
-
-function addZero(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
-}
+function addZero(i) { if (i < 10) { i = "0" + i } return i }
