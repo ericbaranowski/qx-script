5d457cfa89288d1e0532034be42324591dc6491a
Phạm Duy <PHD051199@gmail.com>
2020-08-25 12:45:52 +0700

    bodyFit.js

--- QuanX/scripting/bodyFit.js
+++ QuanX/scripting/bodyFit.js
@@ -1,6 +1,9 @@
 /*
-  https:\/\/subscription-api\.bodybuilding\.com\/entitlements\/check url script-response-body https://raw.githubusercontent.com/phd91105/Scripts/master/QuanX/scripting/bodyFit.js
-  hostname = subscription-api.bodybuilding.com
+
+https:\/\/subscription-api\.bodybuilding\.com\/entitlements\/check url script-response-body https://raw.githubusercontent.com/phd91105/Scripts/master/QuanX/scripting/bodyFit.js
+  
+hostname = subscription-api.bodybuilding.com
+  
 */
 
 var obj = JSON.parse($response.body);
