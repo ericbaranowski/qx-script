022ae7417fc759747d8cae10be5b09be26127b07
Phạm Duy <PHD051199@gmail.com>
2020-09-11 22:08:49 +0700

    doc.js

---
+++ QuanX/scripting/doc.js
@@ -0,0 +1,33 @@
+
+var obj= {  
+  "receiptStatus": "ok",
+  "isEligibleForIntroPeriod": false,
+  "productId": "com.readdle.ReaddleDocsIPad.subscription.year90_nus",
+  "subscriptionAutoRenewStatus": "autoRenewOff",
+  "isInGracePeriod": false,
+  "originalTransactionId": "390000458051403",
+  "subscriptionExpirationDate": "14:54 18/09/2099",
+  "subscriptionState": "trial",
+  "inAppPurchased": [
+    "com.readdle.ReaddleDocsIPad.subscription.year90_nus"
+  ],
+  "receiptId": 1588809109000,
+  "isDocuments6User": true,
+  "inAppStates": [
+    {
+      "receiptStatus": "ok",
+      "isEligibleForIntroPeriod": false,
+      "productId": "com.readdle.ReaddleDocsIPad.subscription.year90_nus",
+      "subscriptionAutoRenewStatus": "autoRenewOff",
+      "isInGracePeriod": false,
+      "originalTransactionId": "390000458051403",
+      "subscriptionExpirationDate": "14:54 18/09/2099",
+      "subscriptionState": "trial",
+      "productName": "subscription"
+    }
+  ],
+  "chargingPlatform": "iOS AppStore",
+  "bundleId": "com.readdle.ReaddleDocsIPad"
+};
+
+$done({body: JSON.stringify(obj)});
