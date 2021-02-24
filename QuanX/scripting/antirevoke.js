528555ff2fe6b3f5d0452e0dc6e7b1fbcc2970eb
Phạm Duy <PHD051199@gmail.com>
2021-02-24 23:07:56 +0700

    update all

--- QuanX/scripting/antirevoke.js
+++
@@ -1,1 +0,0 @@
-var domains={"ocsp.int-x3.letsencrypt.org":1,"ocsp.apple.com":1,"gdmf.apple.com":1,"mesu.apple.com":1,"swscan.apple.com":1,"appldnld.apple.com":1,"world-gen.g.aaplimg.com":1},proxy="PROXY 127.0.0.1:1080;",direct="DIRECT;";function FindProxyForURL(o,p){var e;do{if(domains.hasOwnProperty(p))return proxy;e=p.indexOf(".")+1,p=p.slice(e)}while(e>=1);return direct}
